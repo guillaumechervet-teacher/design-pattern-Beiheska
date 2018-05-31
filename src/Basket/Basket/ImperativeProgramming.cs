798fcfdb946a1d05603f6244b73817e5e675db17
Maxence Branswick <maxence.branswick@epsi.fr>
2018-05-31 13:04:00 +0200

    [EVO] Extraction du code d’appel au JSON

--- src/Basket/Basket/ImperativeProgramming.cs
+++ src/Basket/Basket/ImperativeProgramming.cs
@@ -15,27 +15,21 @@ namespace Basket
             foreach (var basketLineArticle in basketTestBasketLineArticles)
             {
                 // Retrive article from database
-                var codeBase = Assembly.GetExecutingAssembly().CodeBase;
-                var uri = new UriBuilder(codeBase);
-                var path = Uri.UnescapeDataString(uri.Path);
-                var assemblyDirectory = Path.GetDirectoryName(path);
-                var jsonPath = Path.Combine(assemblyDirectory, "article-database.json");
-                IList<ArticleDatabase> articleDatabases =
-                    JsonConvert.DeserializeObject<List<ArticleDatabase>>(File.ReadAllText(jsonPath));
-                var article = articleDatabases.First(articleDatabase =>
-                    articleDatabase.Id == basketLineArticle.Id);
+                string id = basketLineArticle.Id;
+                var article = GetArticleDatabase(id);
                 // Calculate amount
                 var amount = 0;
+                int articlePrice = article.Price;
                 switch (article.Category)
                 {
                     case "food":
-                        amount += article.Price * 100 + article.Price * 12;
+                        amount += articlePrice * 100 + articlePrice * 12;
                         break;
                     case "electronic":
-                        amount += article.Price * 100 + article.Price * 20 + 4;
+                        amount += articlePrice * 100 + articlePrice * 20 + 4;
                         break;
                     case "desktop":
-                        amount += article.Price * 100 + article.Price * 20;
+                        amount += articlePrice * 100 + articlePrice * 20;
                         break;
                 }
 
@@ -43,6 +37,19 @@ namespace Basket
             }
 
             return amountTotal;
+        }
+
+        private static ArticleDatabase GetArticleDatabase(string id)
+        {
+            var codeBase = Assembly.GetExecutingAssembly().CodeBase;
+            var uri = new UriBuilder(codeBase);
+            var path = Uri.UnescapeDataString(uri.Path);
+            var assemblyDirectory = Path.GetDirectoryName(path);
+            var jsonPath = Path.Combine(assemblyDirectory, "article-database.json");
+            IList<ArticleDatabase> articleDatabases =
+                JsonConvert.DeserializeObject<List<ArticleDatabase>>(File.ReadAllText(jsonPath));
+            var article = articleDatabases.First(articleDatabase => { return articleDatabase.Id == id; });
+            return article;
         }
     }
 }
